37640b06ff13b21437b2445ecabf5e1da32c290d
Katy Senia <katy.develops@gmail.com>
2022-08-25 18:32:43 -0400

    useState

--- nucampsite/src/features/campsites/campsitesSlice.js
+++ nucampsite/src/features/campsites/campsitesSlice.js
@@ -2,8 +2,8 @@ import { CAMPSITES } from "../../app/shared/CAMPSITES";
 
 export const selectAllCampsites = () => {
     return CAMPSITES
-};
-
-export const selectRandomCampsite = () => {
-    return CAMPSITES[Math.floor(CAMPSITES.length * Math.random())];
 }
+
+export const selectCampsiteById = (id) => {
+    return CAMPSITES.find((campsite) => campsite.id === id);
+};
