46b3435b4d75cd766326c255ee5ff1409c3cdc42
Katy Senia <katy.develops@gmail.com>
2022-09-02 23:43:54 -0400

    Added regex for error validation

--- nucampsite/src/utils/validateContactForm.js
+++ nucampsite/src/utils/validateContactForm.js
@@ -10,19 +10,21 @@ export const validateContactForm = (values) => {
   }
 
   if (!values.lastName) {
-    errors.firstName = "Required";
+    errors.lastName = "Required";
   } else if (values.lastName.length < 2) {
-    errors.firstName = "Must be at least 2 characters.";
+    errors.lastName = "Must be at least 2 characters.";
   } else if (values.lastName.length > 15) {
-    errors.firstName = "Must be 15 characters or less";
+    errors.lastName = "Must be 15 characters or less";
   }
 
   const reg = /^\d+$/;
   if (!reg.test(values.phoneNum)) {
-    errors.phoneNum = "This phone number should contain only numbers.";
+    errors.phoneNum = "The phone number should contain only numbers.";
   }
 
   if (!values.email.includes("@")) {
     errors.email = "Email should contain a @";
   }
+
+  return errors;
 };
