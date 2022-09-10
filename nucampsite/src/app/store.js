1d0932e9d1679e88eac30e0a217d6f74d69bb388
Katy Senia <katy.develops@gmail.com>
2022-09-10 12:59:40 -0400

    Updated Login Button and Form

--- nucampsite/src/app/store.js
+++ nucampsite/src/app/store.js
@@ -1,14 +1,19 @@
 import { configureStore } from "@reduxjs/toolkit";
+import logger from "redux-logger";
 import { campsitesReducer } from "../features/campsites/campsitesSlice";
 import { commentsReducer } from "../features/comments/commentsSlice";
 import { partnersReducer } from "../features/partners/partnersSlice";
 import { promotionsReducer } from "../features/promotions/promotionsSlice";
+import { userReducer } from "../features/user/userSlice.js";
 
 export const store = configureStore({
-  reducer: {
-    campsites: campsitesReducer,
-    comments: commentsReducer,
-    partners: partnersReducer,
-    promotions: promotionsReducer,
-  },
+	reducer: {
+		campsites: campsitesReducer,
+		comments: commentsReducer,
+		partners: partnersReducer,
+		promotions: promotionsReducer,
+		user: userReducer,
+	},
+	middleware: (getDefaultMiddleware) =>
+		getDefaultMiddleware().concat([logger]),
 });
