fe33329dfb78c682ba86567a68dbfb4d95075f3b
Katy Senia <katy.develops@gmail.com>
2022-09-15 02:10:06 -0400

    Async Partners

--- nucampsite/src/features/partners/partnersSlice.js
+++ nucampsite/src/features/partners/partnersSlice.js
@@ -1,21 +1,54 @@
-import { createSlice } from "@reduxjs/toolkit";
-import { PARTNERS } from "../../app/shared/PARTNERS";
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+//import { PARTNERS } from "../../app/shared/oldData/PARTNERS";
+import { baseUrl } from "../../app/shared/baseUrl";
+import { mapImageUrl } from "../../utils/mapImageUrl";
+
+export const fetchPartners = createAsyncThunk(
+	"partners/fetchPartners",
+	async () => {
+		const response = await fetch(baseUrl + "partners");
+		if (!response.ok) {
+			return Promise.reject(
+				"Unable to fetch, status: " + response.status
+			);
+		}
+		const data = await response.json();
+		return data;
+	}
+);
 
 const initialState = {
-  partnersArray: PARTNERS,
+	partnersArray: [],
+	isLoading: true,
+	errMsg: "",
 };
 
 const partnersSlice = createSlice({
-  name: "partners",
-  initialState,
+	name: "partners",
+	initialState,
+	reducers: {},
+	extraReducers: {
+		[fetchPartners.pending]: (state) => {
+			state.isLoading = true;
+		},
+		[fetchPartners.fulfilled]: (state, action) => {
+			state.isLoading = false;
+			state.errMsg = "";
+			state.partnersArray = mapImageUrl(action.payload);
+		},
+		[fetchPartners.rejected]: (state, action) => {
+			state.isLoading = false;
+			state.errMsg = action.error ? action.error.message : "Fetch failed";
+		},
+	},
 });
 
 export const partnersReducer = partnersSlice.reducer;
 
 export const selectAllPartners = (state) => {
-  return state.partners.partnersArray;
+	return state.partners.partnersArray;
 };
 
 export const selectFeaturedPartner = (state) => {
-  return state.partners.partnersArray.find((campsite) => campsite.featured);
+	return state.partners.partnersArray.find((campsite) => campsite.featured);
 };
